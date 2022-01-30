bf94214727631fec3172d9e1a1ee7d2fe511712a
Seba <sebastianserranob11@gmail.com>
2022-01-30 17:49:53 -0300

    comentarios

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,7 +3,10 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("esto lo escribi a la madrugada");
+	alert("esto lo escribi a la madrugada"); 
+	// alert es la ventana emergente que tiene un mensaje y un boton de ok , el mensaje sera
+	// el que le enviemos a traves de () "ventana modal"
+	// alert es una funcion nativa de js 
 }
 
 	
--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -3,6 +3,22 @@ Debemos lograr tomar un nombre con 'prompt'
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	alert("ok");
-}
 
+	
+	let nombre;         //declaramos un espacio en la memoria del computadorn, la variable nombre
+					   // le asignamos , la inicializamos con el string "cadena alfanumerica"
+						//alert(nombre);      // a la funcion predefinida por el lenguaje le enviamos la variable nombre
+
+						// idealmente let nombre = "messi"; separarlo en dos lineas o escribirlo en una 
+						// depende
+						// dos barras *// son comentario , el programa no las ejecuta , podemos dejar
+						// en suspenso una linea de codigo para probar otra sin tener que eliminarla,
+nombre = prompt("ingrese su nombre");
+
+alert(nombre);
+
+
+
+		//prompt es tambien una funcion nativa de js , nos da una ventana emergente pero nos permite escribir un
+				// dato en esa ventana y me permite darle ok o cancelar luego nos devuelve el dat
+}
