880318bf532cad67eef42f6d6c964bffe1049a83
Seba <sebastianserranob11@gmail.com>
2022-02-03 19:42:16 -0300

    tp2 ej 2

--- 8-TPs/02-jsFerreteConstruccion.js
+++ 8-TPs/02-jsFerreteConstruccion.js
@@ -1,3 +1,7 @@
+/* Serrano Sebastian -Division Z
+- TP 2
+
+
 /*2.	Para el departamento de Construcción:
 A.	 mostrar la cantidad de alambre a comprar  si se ingresara el largo y el ancho de un terreno rectangular y se debe alambra con tres hilos de alambre.
 B.	mostrar la cantidad de alambre a comprar  si se ingresara el radio  de un terreno circular y se debe alambra con tres hilos de alambre.
@@ -6,11 +10,39 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+    let largo,
+    let ancho,
+    let perimetro,
+    let alambre,
 
-}
+    largo = document.getElementById("txtIdLargo").value;
+    largo = parseFloat(largo);
+    
+    ancho = document.getElementById("txtIdAncho").value;
+    ancho = parseInt(ancho);
+
+    perimetro = ancho + largo;
+    alambre = perimetro * 3;
+
+    alert("La cantidad necesaria es de: " + alambre);
+
+} // no sucede nada , me falta el dato del radio... pero necesito el radio para esto?
 function Circulo () 
-{
-	
+{  
+    let radio;
+    let perimetro;
+    let alambre;
+
+    radio = document.getElementById("txtIdRadio").value;
+    radio = parseFloat(radio);
+
+    perimetro = 2 * math.pi * radio; //como se utiliza correctamente?
+    alambre = perimetro * 3;
+
+    alert("la cantidad necesaria es de: " + alambre);
+     
+    // como calculamos el perimetro del circulo en este caso ?
+    // ademas me estan quedando afuera largo y ancho , intuyo hay que usarlos
 }
 function Materiales () 
 {
